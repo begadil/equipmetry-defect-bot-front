91a2fc7865d583bb09994e340ba446122a97ca8b
begadil <begadilseka@gmail.com>
2022-12-06 13:25:12 +0600

    Add header

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,29 @@
 import Vue from "vue";
-import Vuetify from "vuetify/lib/framework";
+import "vuetify/dist/vuetify.min.css";
+import Vuetify from "vuetify";
+import ru from "vuetify/es5/locale/ru";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#23314b",
+      },
+    },
+  },
+  lang: {
+    locales: {
+      ru,
+    },
+    current: "ru",
+  },
+  icons: {
+    values: {
+      equipmetryIcon: {
+        component: () => import("@/components/icons/EquipmetryIcon"),
+      },
+    },
+  },
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,39 @@
 <template>
   <div>
-    <v-btn @click="close">Закрыть</v-btn>
+    <v-card height="50px" tile dark>
+      <v-card-title class="primary align-center">
+        <v-icon color="white" size="39">$vuetify.icons.equipmetryIcon</v-icon>
+        <div class="ml-auto text--white text-truncate user-info">
+          <template v-if="user">
+            {{ user["first_name"] }} {{ user["last_name"] }}
+          </template>
+        </div>
+      </v-card-title>
+    </v-card>
   </div>
 </template>
 
 <script>
 export default {
   name: "HomeView",
-  methods: {
-    close() {
-      this.$telegram.close();
+  computed: {
+    user() {
+      if (this.$telegram.initDataUnsafe) {
+        if (this.$telegram.initDataUnsafe.user) {
+          return this.$telegram.initDataUnsafe.user;
+        }
+      }
+      return null;
     },
   },
 };
 </script>
+
+<style scoped lang="scss">
+.user-info {
+  font-family: "Roboto", sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  max-width: 65vw;
+}
+</style>
